552b20e79fd44d861ec1b80844c49d7364cfa9c6
sanketmhaiske <sanketmhaiske2017@gmail.com>
2023-09-27 15:25:49 +0530

    Added questions

--- js/practiceTest.js
+++ js/practiceTest.js
@@ -4,66 +4,165 @@ document.querySelector('.testName').innerHTML = `
   Practice Test ${practiceTestNo}
 `
 
-function startTest() {
+const QArray = [
+
+    {
+        Qno: 1,
+        Question: 'In the context of computing, what is the full form of URL ?',
+        Options: [
+            {
+                opt_no_1: 'Undistributed Resource Locator',
+                opt_no_2: 'Unified Resource Locator',
+                opt_no_3: 'Uniform Resource Locator',
+                opt_no_4: 'Uniform Region Locator',
+            }
+        ],
+        correctOpt: 3,
+    },
+    {
+        Qno: 2,
+        Question: 'Which of the following is NOT an anti-virus software ?',
+        Options: [
+            {
+                opt_no_1: 'Avast',
+                opt_no_2: 'Linux',
+                opt_no_3: 'Norton',
+                opt_no_4: 'Kaspersky',
+            }
+        ],
+        correctOpt: 2
+    },
+    {
+        Qno: 3,
+        Question: 'In the context of digital computer, which of the following pairs of digits is referred to as binary code ?',
+        Options: [
+            {
+                opt_no_1: '3 and 4 ',
+                opt_no_2: '0 and 1 ',
+                opt_no_3: '2 and 3 ',
+                opt_no_4: '1 and 2 ',
+            }
+        ],
+        correctOpt: 2
+    },
+    {
+        Qno: 4,
+        Question: 'In the context of digital computer, which of the following pairs of digits is referred to as binary code ?',
+        Options: [
+            {
+                opt_no_1: '3 and 4 ',
+                opt_no_2: '0 and 1 ',
+                opt_no_3: '2 and 3 ',
+                opt_no_4: '1 and 2 ',
+            }
+        ],
+        correctOpt: 2
+    },
+    {
+        Qno: 5,
+        Question: 'In the context of digital computer, which of the following pairs of digits is referred to as binary code ?',
+        Options: [
+            {
+                opt_no_1: '3 and 4 ',
+                opt_no_2: '0 and 1 ',
+                opt_no_3: '2 and 3 ',
+                opt_no_4: '1 and 2 ',
+            }
+        ],
+        correctOpt: 2
+    }
+
+]
+
+const correctAns = [];
+const wrongAns = [];
+
+function startTest(Q_no = 0) {
+    var Ques_no = Q_no;
+    const Data = QArray[Ques_no]
+
+    question_numbers = ''
+    for (key in QArray) {
+        question_numbers += `<span style='${correctAns.includes(QArray[key].Qno) ? "color: red;" : ''}'>${QArray[key].Qno}</span>`
+    }
+
     document.querySelector('.testInfo').style.display = 'none';
     document.querySelector('.practiceTest').style.display = 'flex';
     document.querySelector('.practiceTest').innerHTML = `
     <div class="questionArea">
         <div class="question">
-            <strong><span style='font-size: 24px;'>Q.</span> In the context of computing, what is the full form of URL ?</strong>
+            <strong><span style='font-size: 24px;'>Q ${Data.Qno}.</span> ${Data.Question}</strong>
             <form>
-                <div class="opt">
-                    <input type="radio" id="opt1" name="opt" value="opt1" onclick="submitAns('opt1')"/>
-                    <label for="opt1" class='opt1'>Undistributed Resource Locator </label>
-                </div>
-                <div class="opt">
-                    <input type="radio" id="opt2" name="opt" value="opt2" onclick="submitAns('opt2')"/>
-                    <label for="opt2" class='opt2'>Unified Resource Locator </label>
-                </div>
-                <div class="opt">
-                    <input type="radio" id="opt3" name="opt" value="opt3" onclick="submitAns('opt3')"/>
-                    <label for="opt3" class='opt3'>Uniform Resource Locator</label>
-                </div>
-                <div class="opt">
-                    <input type="radio" id="opt4" name="opt" value="opt4" onclick="submitAns('opt4')"/>
-                    <label for="opt4" class='opt4'>Uniform Region Locator</label>
-                </div>
+            ${Data.Options.map(val => (
+        `
+                    <div class="opt">
+                        <input type="radio" id="opt1" name="opt" value="opt1" onclick="submitAns('1' ,'${Data.correctOpt}', '${Data.Qno}')"/>
+                        <label for="opt1" class='opt1'>${val.opt_no_1}</label>
+                    </div>
+                    <div class="opt">
+                        <input type="radio" id="opt2" name="opt" value="opt2" onclick="submitAns('2' , '${Data.correctOpt}', '${Data.Qno}')"/>
+                        <label for="opt2" class='opt2'>${val.opt_no_2}</label>
+                    </div>
+                    <div class="opt">
+                        <input type="radio" id="opt3" name="opt" value="opt3" onclick="submitAns('3','${Data.correctOpt}', '${Data.Qno}')"/>
+                        <label for="opt3" class='opt3'>${val.opt_no_3}</label>
+                    </div>
+                    <div class="opt">
+                        <input type="radio" id="opt4" name="opt" value="opt4" onclick="submitAns('4','${Data.correctOpt}', '${Data.Qno}')"/>
+                        <label for="opt4" class='opt4'>${val.opt_no_4}</label>
+                    </div>
+                `
+    ))}
             </form>
         </div>
-        <button class="nextQueBtn">NEXT QUESTION</button>
+        <button class="nextQueBtn" onclick='nextQue(${Ques_no})'>NEXT QUESTION</button>
     </div>
 
     <div class="progress">
         <h2>YOUR PROGRESS</h2>
         <span>Answer 12 questions correctly.</span>
         <div class="checkAns">
-            <span>1</span>
-            <span>2</span>
-            <span>3</span>
-            <span>4</span>
-            <span>5</span>
-            <span>6</span>
-            <span>7</span>
-            <span>8</span>
-            <span>9</span>
-            <span>10</span>
-            <span>11</span>
-            <span>12</span>
-            <span>13</span>
-            <span>14</span>
-            <span>15</span>
+          ${question_numbers}
         </div>
     </div>
 `
 }
 
-function submitAns(opt) {
-    if (opt == 'opt3') {
-        document.querySelector(`.${opt}`).style.color = 'white';
-        document.querySelector(`.${opt}`).style.background = 'green';
+
+
+
+
+
+function nextQue(Ques_no) {
+    startTest(Ques_no + 1)
+}
+
+
+
+function submitAns(opt, cOpt, Qno) {
+
+    console.log('correct', correctAns)
+    console.log('wrong', wrongAns)
+
+    if (opt == cOpt) {
+        document.querySelector(`.opt${opt}`).style.color = 'white';
+        document.querySelector(`.opt${opt}`).style.background = 'green';
+
+        if (wrongAns.includes(Qno)) {
+            return wrongAns.filter(item => item !== Qno)
+        }
+        if (!correctAns.includes(Qno)) {
+            correctAns.push(Qno)
+        }
     } else {
-        document.querySelector(`.${opt}`).style.color = 'white';
-        document.querySelector(`.${opt}`).style.background = '#ff6e00';
+        if (correctAns.includes(Qno)) {
+            return correctAns.filter(item => item !== Qno)
+        }
+        if (!wrongAns.includes(Qno)) {
+            wrongAns.push(Qno)
+        }
+        document.querySelector(`.opt${opt}`).style.color = 'white';
+        document.querySelector(`.opt${opt}`).style.background = '#ff6e00';
     }
 }
 
@@ -78,7 +177,7 @@ for (let i = 1; i <= 3; i++) {
             <button>START</button>
         </span>
         <div style="display: flex; flex-direction: column;">
-            <strong>${20 + (i*5)}</strong>
+            <strong>${20 + (i * 5)}</strong>
             <span style="font-size: 14px;
             margin: 10px 0;
             color: #454545;">QUESTIONS</span>
